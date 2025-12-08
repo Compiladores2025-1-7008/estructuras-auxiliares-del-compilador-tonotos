158ae89cfdb27433fb0a07865633bc426b029ecd
Mauricio Flores <mauriciofm341@gmail.com>
2025-12-07 18:23:50 -0600

    Implementacion de CodeGenerator

--- src/CodeGenerator.cpp
+++ src/CodeGenerator.cpp
@@ -1,1 +1,18 @@
 #include "CodeGenerator.hpp"
+#include <string>
+
+std::string CodeGenerator::newTemp() {
+    // Generamos "t0", "t1", "t2", ... en cada llamada.
+    return "t" + std::to_string(nextTemp++);
+}
+
+std::string CodeGenerator::newLabel() {
+    // Generamos "L0", "L1", "L2", ... en cada llamada.
+    return "L" + std::to_string(nextLabel++);
+}
+
+void CodeGenerator::reset() {
+    // Reiniciamos ambos contadores.
+    nextTemp = 0;
+    nextLabel = 0;
+}
